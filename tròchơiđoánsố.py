de9c8beed7189e076378172e16b59b973a7d330f
Lê Minh Nhật <111627866+minhnhat2002@users.noreply.github.com>
2023-01-14 00:26:35 +0700

    Update tròchơiđoánsố.py
    Signed-off-by: Lê Minh Nhật <111627866+minhnhat2002@users.noreply.github.com>

--- tròchơiđoánsố.py
+++ tròchơiđoánsố.py
@@ -1,10 +1,17 @@
 import random
 print(("Chọn các số từ 0 tới 100 , nếu bạn đoán đúng bạn sẽ thắng. "))
-def thongtin_nhap(nhap_so): 
+def thongtin_nhap(game_so): 
     thongtin_raw=input("Nhập vào số bạn chọn: ")
     if thongtin_raw.isnumeric():
+    #đang kiểm tra giá trị nhập vào của người dùng (được lưu trong biến "thongtin_raw") có phải là số hay không. 
+    #Phương thức "isnumeric()" được sử dụng để kiểm tra xem một chuỗi chỉ chứa các ký tự số (0-9) hay không. 
+    #Nếu đầu vào là số, chương trình sẽ tiến hành bước tiếp theo, đó là kiểm tra xem số đó có nằm trong phạm vi hợp lệ (0-100) hay không. 
+    #Nếu nhập không phải là số, chương trình sẽ hiển thị thông báo lỗi và yêu cầu người dùng nhập lại số hợp lệ.
         thongtin=int(thongtin_raw)
-        if thongtin in nhap_so:
+        if thongtin in game_so:
+        #kiểm tra xem số mà người dùng nhập vào có nằm trong phạm vi hợp lệ hay không
+        #Biến "game_so" là danh sách các số từ 0 đến 100 nên nếu thông tin nhập của người dùng (được lưu trong biến "thongtin") 
+        #nằm trong danh sách đó thì có nghĩa là thông tin nhập hợp lệ và nằm trong khoảng giá trị hợp lệ.
             return thongtin
         else:
             print("Số bạn vừa nhập không hợp lệ , vui lòng nhập lại!")
@@ -20,12 +27,12 @@ def dieukien(thongtin_nguoichoi,so_game):
         print("Số của bạn lớn hơn số của game đưa ra !")
 
 def thongtin_game(con_so):
-    nhap_so=[_ for _ in range(0, 101)]
-    so_game=random.choice(nhap_so)
+    game_so=[_ for _ in range(0, 101)]
+    so_game=random.choice(game_so)
     
     a=0
     while a<con_so:
-        thongtin_nguoichoi=thongtin_nhap(nhap_so)
+        thongtin_nguoichoi=thongtin_nhap(game_so)
         if thongtin_nguoichoi==-1:
             print("Thử lại 1 số khác: ")
         elif so_game==thongtin_nguoichoi:
